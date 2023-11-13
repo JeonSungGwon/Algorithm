4df8da02493ed067feab77599711e81ae50551f5
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2023-11-14 03:19:17 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 114264 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -6,24 +6,20 @@ N, M = map(int, input().split())
 visited = [[False] * M for _ in range(N)] 
 
 matrix = [list(map(int, input().strip())) for _ in range(N)]
-dx = [0, 1, -1, 0]
-dy = [1, 0, 0, -1]
-
-def BFS(x, y, count):
-    my_que.append((x, y, count))
+dx = [0,1,-1,0]
+dy = [1,0,0,-1]
+def BFS(x,y,count):
+    my_que.append((x,y,count))
     visited[x][y] = True
-
     while my_que:
         ax, ay, count = my_que.popleft()
-
-        if ax == N - 1 and ay == M - 1:
+        if ax == N -1 and ay == M - 1:
             return count
-
         for i in range(4):
             nx, ny = ax + dx[i], ay + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and matrix[nx][ny] == 1 and not visited[nx][ny]:
-                my_que.append((nx, ny, count + 1))
-                visited[nx][ny] = True  # 큐에 새로운 정보를 추가할 때에만 visited를 설정하도록 변경
+            if nx >= 0 and ny >= 0 and nx < N and ny < M and matrix[nx][ny] == 1 and not visited[nx][ny]:
+                my_que.append((nx, ny, count+1))
+                visited[nx][ny] = True
 
-my_count = BFS(0, 0, 1)
+my_count = BFS(0,0,1)
 print(my_count)
