d19557e0d6fc55682a7128d108b83bcd845c58a6
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2023-11-18 02:46:48 +0900

    [D3] Title: N-Queen, Time: 250 ms, Memory: 61,424 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,30 +1,25 @@
-T = int(input())
-def DFS(x, x_list, y_list):
-    global count
-    if len(x_list) == N:
-        count += 1
-    else:
-        for k in range(N):
-            di = True
-            for ax, ay in zip(x_list, y_list):
-                for m in range(1, N):
-                    if (ax == x - m and ay == k - m) or (ax == x - m and ay == k + m):
-                        di = False
-            if not di:
-                continue
-            elif k in y_list:
-                continue
-            else:
-                x_list.append(x)
-                y_list.append(k)
-                DFS(x + 1, x_list, y_list)
-                x_list.pop()
-                y_list.pop()
+def check(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
+            return False
+    return True
 
-for i in range(1, T + 1):
-    count = 0
+
+def DFS(x):
+    global result
+    if x == N:
+        result += 1
+    else:
+        for i in range(N):
+            row[x] = i
+            if check(x):
+                DFS(x + 1)
+
+T = int(input())
+
+for i in range(1, T+1):
     N = int(input())
-    total_count = 0
-    for j in range(N):
-        DFS(1, [0], [j])
-    print("#{} {}".format(i, count))
+    row = [0] * N
+    result = 0
+    DFS(0)
+    print("#{} {}".format(i, result))
