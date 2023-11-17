ac7c650a9939ca3157dbf9f59ff4a116335784b5
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2023-11-18 01:58:43 +0900

    [Gold IV] Title: N-Queen, Time: 12032 ms, Memory: 137132 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,45 +1,61 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10 ** 4)
 
-def setQueen(R) :
-    global queens, C
-    
-    # 대각선으로 충돌 확인
-    if len(queens) > 1 :
-        for i in range(len(queens)-1) :
-            # 한 퀸으로부터 (+k, +-k) 만큼 떨어져있다면 충돌, return
+"""
+# Backtrack 함수의 작동 방식
+(1) 현재 Row가 보드판 사이즈인 n보다 크면 가능한 방법 한 가지를 찾았다 생각하고 1을 리턴한다.
+(2) 지역변수 solutions를 0으로 초기화. solutions는 현재 보드 상태에서 가능한 해결방법의 수를 나타냄
+(3) 현재 Row의 모든 Columns에 대해 순회를 돌면서 퀸을 놓을 수 있는지 여부를 살펴본다.
+    - 현재 칸의 대각선, anti-대각선을 계산한다.
+    - 현재 칸의 열, 대각선, anti-대각선에 위치한 퀸이 있으면 퀸을 놓을 수 없으므로 다음 컬럼으로 넘어간다.
+    - 현재 칸에 퀸을 놓을 수 있다면 3개의 set(cols, diagonals, anti_diagonals)를 업데이트 하고, row+1 파라미터로 함수를 호출한다.
+(4) 탐색을 완료하면 되돌아가면서(backtrack) 퀸을 회수한다. 즉, set에 추가한 값들을 제거한다.
+"""
 
-            # 새로 투입된 퀸만이 조건을 만족하고 들어온 이전 퀸들과만 비교하여 충돌하는지 판별하여 연산 최적화
-            k = queens[-1][0]-queens[i][0] # k > 0
+def n_queens(n):
+    # diagonals: 대각선으로 놓인 퀸들의 위치 정보 (row - col)
+    # anti_diagonals: 역방향 대각선으로 놓인 퀸들의 정보 (row + col)
+    # cols: 배열의 인덱스 = Row. 각 행의 몇 번째 컬럼에 퀸이 위치하는지에 대한 정보
+    def backtrack(row, diagonals, anti_diagonals, cols):
+        # Base Case: 현재 Row가 보드판 사이즈인 n이 되면 가능한 방법 한 가지를 찾았다 생각하고 1을 리턴한다.
+        if row == n:
+            return 1
 
-            if queens[i][1] + k == queens[-1][1] or queens[i][1] - k == queens[-1][1] : return
+        # 지역변수 solutions의 값을 0으로 초기화. solutions는 현재 보드 상태에서 가능한 해결방법의 수를 나타냄
+        solutions = 0
+        
+        for col in range(n):
+        	# 오른쪽 아래로 내려가는 대각선은 같은 대각선에 있는 칸들의 row-col 값이 같다.
+            curr_diagonal = row - col
+            # 오른쪽 위로 올라가는 대각선은 같은 대각선에 있는 칸들의 row+col 값이 같다.
+            curr_anti_diagonal = row + col
 
-    # 조건 충족 및 배치 완료
-    if len(queens) == N :
-        global setCase
-        setCase += 1
-        return
-    
-    # 퀸은 각 행/열에 하나씩 존재할 것
-    for c in range(N) :
-        if C[c] : continue
+            # 만약 현재 칸의 열, 대각선, anti-대각선에 놓인 퀸이 있으면 퀸을 놓을 수 없으므로 다음 컬럼으로 넘어간다.
+            if (col in cols
+                    or curr_diagonal in diagonals
+                    or curr_anti_diagonal in anti_diagonals):
+                continue
 
-        # 추가
-        queens.append((R,c))
-        C[c] = True
-        setQueen(R+1)
-        # 복구
-        queens.pop()
-        C[c] = False
+            # 현재 칸에 퀸을 놓을 수 있다면 3개의 set(cols, diagonals, anti_diagonals)를 업데이트
+            cols.add(col)
+            diagonals.add(curr_diagonal)
+            anti_diagonals.add(curr_anti_diagonal)
+
+            # 보드의 상태가 업데이트된 상태로 다음 행으로 넘어감
+            solutions += backtrack(row + 1, diagonals, anti_diagonals, cols)
+
+            # "Remove" the queen from the board since we have already
+            # explored all valid paths using the above function call
+            # 탐색을 완료하면 되돌아가면서(backtrack) 퀸을 회수한다. 즉, set에 추가한 값들을 제거한다.
+            cols.remove(col)
+            diagonals.remove(curr_diagonal)
+            anti_diagonals.remove(curr_anti_diagonal)
+
+        return solutions
+
+    # 처음 재귀함수를 호출하는 지점
+    return backtrack(0, set(), set(), set())
 
 
-N = int(input())
-
-setCase = 0 # 출력값
-queens = [] # (r0, c0), (r1, c1), ...
-R = 0 # 진행된 행
-C = [False for _ in range(N)] # 할당된 열
-
-setQueen(R)
-
-print(setCase)
+N = int(sys.stdin.readline())
+print(n_queens(N))
