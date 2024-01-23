dab9396599d5eb95e57fd391b36625198347fee9
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-01-23 23:33:52 +0900

    [Silver II] Title: 좌표 압축, Time: 1964 ms, Memory: 238276 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        int[] sortedArr = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArr);
+        
+        int idx = 0;
+        for (int a : sortedArr) {
+            if (!indexMap.containsKey(a)) {
+                indexMap.put(a, idx++);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int a : arr) {
+            int zip = indexMap.get(a);
+            sb.append(zip).append(' ');
+        }
+        System.out.println(sb);
+    }
+}
