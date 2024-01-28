fd1a0aa18718303006692d02f1639bb30dcbc5f9
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-01-28 16:00:33 +0900

    [Silver I] Title: 금민수의 개수, Time: 272 ms, Memory: 19072 KB -BaekjoonHub

--- 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.java
@@ -1,33 +1,34 @@
-
 import java.util.Scanner;
 
 public class Main {
-    static long A;
-    static long B;
-    static long cnt = 0;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        A = sc.nextLong();
-        B = sc.nextLong();
-        String characters = "74";
-        long minLength = Long.toString(A).length();
-        long maxLength = Long.toString(B).length();
-        for (long length = minLength; length <= maxLength; length++) {
-            generateCombinations(characters, (int) length, "");
+	static int A;
+	static int B;
+	static int cnt = 0;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		String characters = "74";
+		int minLength = Integer.toString(A).length();
+		int maxLength = Integer.toString(B).length();
+		for (int length = minLength; length <= maxLength; length++) {
+            generateCombinations(characters, length, "");
         }
-        System.out.println(cnt);
-    }
+		System.out.println(cnt);
+	}
 
-    private static void generateCombinations(String characters, int length, String string) {
-        if (string.length() == length) {
-            if (A <= Long.parseLong(string) && B >= Long.parseLong(string)) {
-                cnt++;
-            }
+	private static void generateCombinations(String characters, int length, String string) {
+		if (string.length() == length) {
+			if(A <= Integer.parseInt(string) && B>= Integer.parseInt(string)) {
+				cnt++;	
+			}
             return;
         }
-        for (int i = 0; i < characters.length(); i++) {
-            generateCombinations(characters, length, string + characters.charAt(i));
+		for (int i = 0; i < characters.length(); i++) {
+			if(B>=Long.parseLong(string + characters.charAt(i))) {
+				generateCombinations(characters, length, string + characters.charAt(i));
+			}
         }
-    }
+	}
+
 }
