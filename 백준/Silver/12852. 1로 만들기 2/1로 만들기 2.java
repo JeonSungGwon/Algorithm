70db7933f917ec292b51ddd131f4b8a674e48fd5
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-01-31 21:40:50 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1204 ms, Memory: 336076 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int X = sc.nextInt();
 		Recursion(X, 0, X+"");
 		System.out.println(result);
 		System.out.println(myStr);
 	}
-	public static int result = 1000000;
+	public static int result = 1000;
 	public static String myStr;
 	public static void Recursion(int currentX, int cnt, String str) {
 		
@@ -21,7 +19,7 @@ public class Main {
 			}
 			return;
 		}
-        if(cnt >= result) return;
+		if(cnt >= result) return;
 		if(currentX % 3 == 0) {
 			Recursion(currentX/3, cnt + 1, str+" " +currentX/3);
 		}
