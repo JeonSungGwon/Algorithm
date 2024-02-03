b6105234224fcb08535205de269fe4f0cf778e38
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-03 16:16:21 +0900

    [Silver I] Title: 신입 사원, Time: 876 ms, Memory: 299612 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,37 +1,35 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
 
-        int n= scan.nextInt();
-
-        for(int i=0;i<n;i++){
-            int m=scan.nextInt();
-            int count=1;
-            int work[]=new int[m+1];
-            for(int j=0;j<m;j++){
-                int a=scan.nextInt();
-                int b=scan.nextInt();
-                work[a]=b;
-            }
-
-            int vot= work[1];
-
-            for(int j=2;j<=m;j++){
-                if(work[j]<vot){
-                    vot=work[j];
-                    count++;
-
-                }
-            }
-            System.out.println(count);
-
-
-
-        }
-        scan.close();
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(br.readLine());
+		while(tc-->0) {
+			int n = Integer.parseInt(br.readLine());
+			
+			int[] arr = new int[n];
+			for(int i=0; i<n; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a-1] = b;
+			}
+			int count =1;
+			int rating = arr[0];
+			for(int i=1; i<n; i++) {
+				if(rating>arr[i]) {
+					count++;
+					rating = arr[i];
+				}
+			}
+			sb.append(count+"\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
