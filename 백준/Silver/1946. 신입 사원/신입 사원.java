fc913d92adcd907675287c235dcf23fd7a716066
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-03 16:22:22 +0900

    [Silver I] Title: 신입 사원, Time: 864 ms, Memory: 301480 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -4,32 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int tc = Integer.parseInt(br.readLine());
-		while(tc-->0) {
-			int n = Integer.parseInt(br.readLine());
-			
-			int[] arr = new int[n];
-			for(int i=0; i<n; i++) {
-				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				arr[a-1] = b;
+		int T = Integer.parseInt(br.readLine());
+		
+		
+		for(int tc = 1 ; tc<=T ; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			boolean[] isBoolean = new boolean[N];
+			int[] arr = new int[N+1];
+			for(int i = 0 ; i<N ; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				arr[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
 			}
-			int count =1;
-			int rating = arr[0];
-			for(int i=1; i<n; i++) {
-				if(rating>arr[i]) {
-					count++;
-					rating = arr[i];
+			
+			int cnt = 1;
+			int current = arr[1];
+			for(int i = 2 ; i<=N ; i++) {
+				if(current > arr[i]) {
+					cnt++;
+					current=arr[i];
 				}
 			}
-			sb.append(count+"\n");
+			
+			System.out.println(cnt);
+			
 		}
-		System.out.println(sb.toString());
 	}
 }
