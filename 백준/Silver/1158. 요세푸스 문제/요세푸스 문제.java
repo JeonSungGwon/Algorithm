00335b91c0603bb9b1a27aae19b5137cc119485d
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-05 21:06:20 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 176 ms, Memory: 15232 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder output = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer str = new StringTokenizer(input.readLine());
+
+        int N = Integer.parseInt(str.nextToken());
+        int K = Integer.parseInt(str.nextToken());
+
+        List<Integer> yosepus = new LinkedList<>();
+        for(int i = 1; i <= N; i++)
+            yosepus.add(i);
+        int temp = K-1;
+        output.append("<");
+        while(yosepus.size() > 0){
+            temp = temp % yosepus.size();
+            output.append(yosepus.get(temp) +", ");
+            yosepus.remove(temp);
+            temp += K - 1;
+        }
+        output.setLength(output.length()-2);
+        output.append(">");
+        System.out.println(output);
+    }
+}
