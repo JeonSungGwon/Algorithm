33b367161c53521f31fee3aa47766d989b86ac50
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-10 17:35:51 +0900

    [Gold IV] Title: 음식 평론가, Time: 204 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(M - gcd(N, M));
+	}
+	
+	public static int gcd(int a, int b) {
+		if(b == 0)	return a;
+		return gcd(b, a%b);
+	}
+}
+
