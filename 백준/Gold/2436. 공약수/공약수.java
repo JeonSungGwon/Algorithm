8a9c877a4c0fb7531818dbb4651b90df4d3e8b27
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-15 01:48:01 +0900

    [Gold V] Title: 공약수, Time: 236 ms, Memory: 18428 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.java
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -1,38 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int gcd = Integer.parseInt(st.nextToken());
-		int lcd = Integer.parseInt(st.nextToken());
-		
-		long mul = (long)gcd*lcd;
-		
-		int a=0, b=0;
-		
-		for(int i = gcd; i<=Math.sqrt(mul);i+=gcd) {
-			if(mul % i == 0 && euclidean(i,mul / i) == gcd) {
-				a = i;
-				b = (int)(mul / i);
+        long mul = (long)a*b;
+        long num1 = 0;
+        long num2 = 0;
+        
+        for(int i = a; i<=Math.sqrt(mul); i+=a) {
+			if(mul % i == 0 && gcd(i, mul / i) == a) {
+				num1 = i;
+				num2 = (mul / i);
 			}
 		}
-		
-		System.out.println(a + " " + b);
-	}
-	
-	
-	// a와 b의 최대 공약수는 b와 a%b의 최대 공약수와 같다 (유클리드 호제법)
-	public static long euclidean(long a, long b) {
-		long r = a % b;
-		if( r == 0) {
+        
+
+        System.out.println(num1 + " " + num2);
+    }
+
+    public static long gcd(long a, long b) {
+		long c = a % b;
+		if(c == 0) {
 			return b;
 		}
-		return euclidean(b,r);
+		return gcd(b,c);
 	}
-
 }
