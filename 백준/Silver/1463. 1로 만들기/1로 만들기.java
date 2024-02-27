d510c26ae7b0b29f2ed59bcc6b95d0a633a658e4
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-02-27 22:00:13 +0900

    [Silver III] Title: 1로 만들기, Time: 228 ms, Memory: 21596 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] dp;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		dp = new int[n+3];
+		System.out.println(dp(n));
+	}
+	private static int dp(int n) {
+		dp[1] = 0;
+		dp[2] = 1; 
+		dp[3] = 1;
+		for(int i=3; i<=n; i++) {
+			dp[i] = dp[i-1] + 1;
+			if(i%2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
+			}
+			if(i%3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+			}
+		}
+		return dp[n];
+	}
+
+}
