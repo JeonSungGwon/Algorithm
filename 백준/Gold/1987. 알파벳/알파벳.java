8cfb6502f5c0a64f8de89c0a01d1cbb4f7314267
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-06-29 20:29:29 +0900

    [Gold IV] Title: 알파벳, Time: 912 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,53 +1,53 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-	
-	static int R,C;
-	static char[][] board;
-	static int[] dx = {0, 1, -1, 0};
-	static int[] dy = {1, 0, 0, -1};
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		board = new char[R][C];
-		
-		for(int i = 0 ; i < R ; i++) {
-			String str = br.readLine();
-			for(int j = 0 ; j < C ; j++) {
-				board[i][j] = str.charAt(j);
-			}
-		}
-		Set<Character> set = new HashSet<Character>();
-		set.add(board[0][0]);
-		DFS(0,0, set);
-		System.out.println(result);
-	}
-	
-	static int result = 0;
-	private static void DFS(int i, int j, Set<Character> set) {
-		
-		if(set.size() > result) {
-			result = set.size();
-		}
-		
-		for(int idx = 0 ; idx < 4 ; idx++) {
-			int nx = i + dx[idx];
-			int ny = j + dy[idx];
-			if(nx >= 0 && ny >= 0 && nx < R && ny < C) {
-				if (!set.contains(board[nx][ny])) {
-				    set.add(board[nx][ny]);
-				    DFS(nx, ny, set);
-				    set.remove(board[nx][ny]); 
-				}
+    static int R, C;
+    static char[][] board;
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {1, 0, 0, -1};
 
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        board = new char[R][C];
+
+        for (int i = 0; i < R; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+        boolean[] visited = new boolean[26];
+        visited[board[0][0] - 'A'] = true;
+        System.out.println(DFS(0, 0, visited));
+    }
+
+    private static int DFS(int i, int j, boolean[] visited) {
+
+        int maxResult = 1; 
+        
+        for (int idx = 0; idx < 4; idx++) {
+            int nx = i + dx[idx];
+            int ny = j + dy[idx];
+            if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
+                int charIndex = board[nx][ny] - 'A';
+                if (!visited[charIndex]) {
+                    visited[charIndex] = true;
+                    maxResult = Math.max(maxResult, 1 + DFS(nx, ny, visited));
+                    visited[charIndex] = false;
+                }
+            }
+        }
+        
+        return maxResult;
+    }
 }
