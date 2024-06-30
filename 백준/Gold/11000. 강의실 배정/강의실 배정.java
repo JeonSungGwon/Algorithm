cdba9d96c8646b2b33164a54af6285fae5866ffb
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-06-30 19:46:07 +0900

    [Gold V] Title: 강의실 배정, Time: 624 ms, Memory: 70064 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,15 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+	
 	static class Class implements Comparable<Class>{
 		int start;
 		int end;
@@ -18,35 +11,37 @@ public class Main {
 			this.start = start;
 			this.end = end;
 		}
-
+		
 		@Override
 		public int compareTo(Class o) {
+			
 			if(this.start == o.start) {
 				return this.end - o.end;
-			}else return this.start - o.start;
+			}
+			
+			return this.start - o.start;
 		}
 		
 	}
-	public static void main(String[] args) throws IOException{
+	
+	static ArrayList<Class> arr = new ArrayList<Class>();
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = null;
-		List<Class> list = new ArrayList<>();
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			list.add(new Class(start, end));
+			arr.add(new Class(start, end));
 		}
-		
-		Collections.sort(list);
+		Collections.sort(arr);
 		Queue<Integer> q = new PriorityQueue<>();
-		int endTime=0;
-		for(Class m : list) {
-			endTime = m.end;
+		int endTime = 0;
+		for(Class c : arr) {
+			endTime = c.end;
 			
-			if(!q.isEmpty() && q.peek() <= m.start) {
+			if(!q.isEmpty() && q.peek() <= c.start) {
 				q.poll();
 			}
 			q.add(endTime);
