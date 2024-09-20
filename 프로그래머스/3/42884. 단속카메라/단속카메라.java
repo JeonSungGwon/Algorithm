a9f33658a9e2f187488a768d065ecc172b52c6da
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-09-20 15:37:26 +0900

    [level 3] Title: 단속카메라, Time: 11.00 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
-        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
 
         int camera = Integer.MIN_VALUE;
         
