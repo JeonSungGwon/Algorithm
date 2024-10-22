dae0e53bc956721003a6afb694d57de337a0b7c2
JeonSungGwon <129651243+JeonSungGwon@users.noreply.github.com>
2024-10-22 21:49:22 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.25 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -7,7 +7,6 @@ class Solution {
     public String[] solution(String[] orders, int[] course) {
         List<String> answerList = new ArrayList<>();
 
-        // 각 주문 정렬
         for (int i = 0; i < orders.length; i++) {
             char[] chars = orders[i].toCharArray();
             Arrays.sort(chars);
@@ -15,16 +14,15 @@ class Solution {
         }
 
         for (int c : course) {
-            map.clear(); // 매번 조합 길이별로 카운팅 초기화
+            map.clear();
             for (String order : orders) {
                 if (order.length() >= c) {
-                    strArr = new char[c]; // 조합의 크기에 맞게 배열 초기화
-                    createCombination(0, 0, c, order); // 조합 생성
+                    strArr = new char[c]; 
+                    createCombination(0, 0, c, order); 
                 }
             }
 
-            // 가장 많이 선택된 조합 추출
-            int maxCount = 2; // 최소 2번 이상 등장한 조합만 고려
+            int maxCount = 2; 
             List<String> candidates = new ArrayList<>();
             for (Map.Entry<String, Integer> entry : map.entrySet()) {
                 if (entry.getValue() > maxCount) {
@@ -35,15 +33,13 @@ class Solution {
                     candidates.add(entry.getKey());
                 }
             }
-            answerList.addAll(candidates); // 후보들을 정답 리스트에 추가
+            answerList.addAll(candidates); 
         }
 
-        // 결과 정렬 후 배열로 변환
         Collections.sort(answerList);
         return answerList.toArray(new String[0]);
     }
 
-    // 부분 조합을 생성하는 함수
     public static void createCombination(int cnt, int start, int limit, String str) {
         if (cnt == limit) {
             String combination = new String(strArr);
@@ -53,7 +49,7 @@ class Solution {
 
         for (int i = start; i < str.length(); i++) {
             strArr[cnt] = str.charAt(i);
-            createCombination(cnt + 1, i + 1, limit, str); // 재귀 호출로 부분 조합 생성
+            createCombination(cnt + 1, i + 1, limit, str); 
         }
     }
 }
